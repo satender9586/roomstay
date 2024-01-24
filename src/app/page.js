753d673a3206f6211f04ba644b2977cca4513798
suraj <surajgautam56878@gmail.com>
2024-01-24 19:06:15 +0530

    design:working on tab

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import Growth from "./growth/page";
+import Content from "./content/page";
 export default function Home() {
   return (
     <div>
-      <Growth/>
+      <Content/>
     </div>
   );
 }
