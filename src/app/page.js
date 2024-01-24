139380c12180d7fd7a90a1fb7ddeea692b444de4
suraj <surajgautam56878@gmail.com>
2024-01-24 15:41:53 +0530

    design: makeing growth page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import Growth from "./growth/page";
 export default function Home() {
   return (
     <div>
-      <h2>Hii</h2>
+      <Growth/>
     </div>
   );
 }
