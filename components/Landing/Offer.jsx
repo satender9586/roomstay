8510fb7dd3cb7c623adf86cbdd7f392bea9dde94
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-01-24 22:52:37 +0530

    add: offer section responsive done

--- components/Landing/Offer.jsx
+++ components/Landing/Offer.jsx
@@ -5,10 +5,10 @@ import telegramImg from "../../assests/Icons/telegram.webp"
 const Offer = () => {
     return (
         <section className='bg-[#050033]'>
-            <div className='container | font-heading | flex | relative | py-14 md:py-18 '>
+            <div className='container | font-heading | flex | relative | py-16 md:py-18 xl:py-24 '>
 
 
-                <div className=' text-white | text-3xl md:text-8xl font-normal | pb-24 w-[90%]'>
+                <div className=' text-white | text-4xl lg:text-6xl xl:text-8xl font-normal |  w-[50%]'>
                     <span className='text-cyan-300 leading-10'>
                         Ready to power
                     </span>
@@ -20,8 +20,8 @@ const Offer = () => {
 
 
 
-                <div className='relative'>
-                <Image src={telegramImg} unoptimized alt='telegram' height={489} width={157} className='' style={{ width: "auto", height: "auto" }} />
+                <div className='relative flex items-start md:items-center'>
+                <Image src={telegramImg} unoptimized alt='telegram' height={489} width={157} className='w-full md:w[50%] lg:w-full' style={{ width: "auto", height: "auto" }} />
                 </div>
 
 
