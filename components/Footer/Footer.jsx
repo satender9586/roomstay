41538ebe0c0f0669479c87c58cda55cf74cc68e0
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-01-24 23:37:31 +0530

    add:footer responsive

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -4,37 +4,48 @@ import Image from 'next/image'
 
 const Footer = () => {
     return (
-        <footer className='bg-slate-400'>
+        <footer className=' bg-[#1a181e] '>
 
-            <div className='container | bg-[#1a181e] | grid grid-cols-5'>
-                <div className=' flex justify-end | py-12 text-white'>
-                    <Image src={arrowIcon} alt='arrowicon' height={190} width={250} style={{ width: "auto", height: "auto" }} />
+            <div className='container | grid grid-cols-5 | py-4  sm:py-6 md:py-8 lg:py-10 xl:py-12'>
+
+                <div className=' flex justify-center | text-white | row-span-2 md:row-span-1 col-span-5  sm:col-span-1'>
+                    <Image src={arrowIcon} alt='arrowicon' height={190} width={250} style={{ width: "auto", height: "auto" }} className='w-[150px] xl:w-[250px]'/>
 
                 </div>
-                <div className=' flex justify-end | py-12 text-white'>
-                    <div className=' flex flex-col gap-2'>
-                        <div className='text-xl leading-6'>Business Tools</div>
-                        <div className='text-xl leading-6'>Business Tools</div>
-                        <div className='text-xl leading-6'>Business Tools</div>
-                        <div className='text-xl leading-6'>Business Tools</div>
-                        <div className='text-xl leading-6'>Business Tools</div>
+                <div className=' flex justify-center lg:justify-center | text-white py-4 sm:py-2  | col-span-5  sm:col-span-2 md:col-span-1'>
+                    <div className=' flex flex-col gap-3 text-xs md:text-sm lg:text-lg xl:text-xl'>
+                        <div>Jobs</div>
+                        <div>Enquiry</div>
+                        <div>Others</div>
+                        <div>Terms</div>
                     </div>
                 </div>
-                <div className=' flex justify-end | py-12 text-white'>
-                    <div className=' flex flex-col gap-2'>
-                        <div className='text-xl leading-6'>Business Tools</div>
-                        <div className='text-xl leading-6'>Business Tools</div>
-                        <div className='text-xl leading-6'>Business Tools</div>
-                        <div className='text-xl leading-6'>Business Tools</div>
+                <div className=' flex justify-center lg:justify-center | text-white py-4 sm:py-2 | col-span-5 sm:col-span-2 md:col-span-1'>
+                    <div className=' flex flex-col gap-3 text-xs md:text-sm lg:text-lg xl:text-xl'>
+                        <div>About</div>
+                        <div>Contact</div>
+                        <div>Blog</div>
+                        <div>Help</div>
                     </div>
                 </div>
-                <div className=' flex  justify-end | py-12 text-white col-span-2'>
-                    <div className=' flex flex-col gap-2'>
-                        <div className='text-xl leading-6'>Satender - sksatender59@gmail.com</div>
-                        <div className='text-xl leading-6'>Jai Singh - jai.singh.corporate@gmail.com</div>
-                        <div className='text-xl leading-6'>Suraj - suraj.kumar.corporate@gmail.com</div>
+               
+                <div className='flex md:hidden justify-center | text-white py-4 sm:py-2 | col-span-5 sm:col-span-2 md:col-span-2 '>
+                    <div className=' flex flex-col gap-3 text-xs md:text-sm lg:text-lg xl:text-xl'>
+                        <div className='leading-6'>Linked In</div>
+                        <div className='leading-6'>Twitter</div>
+                        <div className='leading-6'>Facebook</div>
                     </div>
                 </div>
+               
+                <div className=' flex justify-center | text-white py-4 sm:py-2 | col-span-5 sm:col-span-2 md:col-span-2 '>
+                    <div className=' flex flex-col gap-3 text-xs md:text-sm lg:text-lg xl:text-xl'>
+                        <div className='leading-6'>Satender - sksatender59@gmail.com</div>
+                        <div className='leading-6'>Jai Singh - jai.singh.corporate@gmail.com</div>
+                        <div className='leading-6'>Suraj - suraj.kumar.corporate@gmail.com</div>
+                    </div>
+                </div>
+
+               
             </div>
             <div className='w-full text-white mb-16 h-1' />
 
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Offer/>
-      {/* <Footer/> */}
+      <Footer/>
     </div>
   );
 }
