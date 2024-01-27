ba178b91900913e4ec00679af8b0f7c0e60e8ce9
satender ahirwar <sksatenderkumar59@gmail.com>
2024-01-27 12:52:19 +0530

    add: image Box

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import FourBoxes from "../../components/Landing/FourBoxes";
 import Offer from "../../components/Landing/Offer";
 import Footer from "../../components/Footer/Footer";
 import Imagebox from "../../components/About/Imagesbox";
+import ImageBox2 from "../../components/About/ImageBox2";
 
 
 export default function Home() {
   return (
     <div>
       <Imagebox/>
+      <ImageBox2/>
       <FourBoxes/>
       <Offer/>
       <Footer/>
