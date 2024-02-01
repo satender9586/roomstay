19959e66c9e2e739573ad0c50c0de444ac79ed6f
satender ahirwar <sksatenderkumar59@gmail.com>
2024-02-01 11:26:36 +0530

    reponsive cart image1

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,14 @@ import Offer from "../../components/Landing/Offer";
 import Footer from "../../components/Footer/Footer";
 import Imagebox from "../../components/About/Imagesbox";
 import ImageBox2 from "../../components/About/ImageBox2";
+import ImageCarts from "../../components/About/ImageCarts";
 
 
 export default function Home() {
   return (
     <div>
-      <Imagebox/>
+      <ImageCarts/>
+      {/* <Imagebox/> */}
       <ImageBox2/>
       <FourBoxes/>
       <Offer/>
