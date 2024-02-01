4fb13d7e22383fb574712d763e56d9744a43feee
satender ahirwar <sksatenderkumar59@gmail.com>
2024-02-01 14:31:48 +0530

    reponseive image cart

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Offer from "../../components/Landing/Offer";
 import Footer from "../../components/Footer/Footer";
 import Imagebox from "../../components/About/Imagesbox";
 import ImageBox2 from "../../components/About/ImageBox2";
-import ImageCarts from "../../components/About/ImageCarts";
+import ImageCarts from "../../components/About/carasoulIageCarts";
 
 
 export default function Home() {
