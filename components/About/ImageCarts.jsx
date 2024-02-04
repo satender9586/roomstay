5785ea3b415ada1f70332f5eb932dacef22484b9
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-02-04 19:09:08 +0530

    resolve: 4th time index fix

---
+++ components/About/ImageCarts.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import React from "react";
+
+const ImageCarts = () => {
+  return (
+    <div className="container">
+      <div className="p-4">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageCarts;
