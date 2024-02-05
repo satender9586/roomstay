00c92bc0798583c46a7f8e958222142e56212f70
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-02-05 17:56:00 +0530

    modify: pricing card design on landing

--- components/Landing/Pricing.jsx
+++ components/Landing/Pricing.jsx
@@ -1,10 +1,14 @@
+import Image from "next/image";
 import React from "react";
 import { CiIceCream } from "react-icons/ci";
+import priceOneImg from "../../assests/Images/price1.png"
+import priceTwoImg from "../../assests/Images/price2.png"
+import priceThreeImg from "../../assests/Images/price3.png"
 
 const Pricing = () => {
   return (
-    <div className="container bg-[#F4F5FA] py-6">
-      <div className="p-4 pb-[3rem]">
+    <div className=" bg-[#ffe9b5] py-6">
+      <div className="p-4 pb-[3rem] container">
         <div class="md:p-4 flex flex-col items-start sm:items-center w-full">
           <h1 class="font-heading font-bold text-2xl sm:text-3xl md:text-4xl lg:text-4xl">
             Get the most out of your
@@ -14,113 +18,29 @@ const Pricing = () => {
             with the right subscription
           </h1>
           <div class="sm:p-4 flex flex-col items-start sm:items-center w-full">
-            <h1 class="font-heading font-normal  text-base sm:text-2xl md:text-2xl lg:text-base opacity-35">
+            <div class="font-heading font-semibold text-black text-base sm:text-2xl md:text-2xl lg:text-base opacity-35">
               All devices come with free delivery on pickup as standard. See
               information on{" "}
               <span class="sm:hidden">
                 available shopping options for your location
               </span>
-            </h1>
-            <h1 class="hidden sm:block font-heading font-normal  text-base sm:text-2xl md:text-2xl lg:text-base opacity-35">
+            </div>
+            <div class="hidden sm:block font-heading font-semibold  text-base sm:text-2xl md:text-2xl lg:text-base opacity-35">
               available shopping options for your location
-            </h1>
+            </div>
           </div>
         </div>
 
         {/*Three Cards */}
         <div class="flex flex-wrap justify-center items-center gap-4 md:gap-5 lg:gap-10 xl:gap-18 py-4 lg:px-[12%]">
           <div class="w-full sm:w-[48%] md:w-[48%] lg:w-[30%] xl:w-[30%] mb-6">
-            <div class="h-[326px] bg-[#FFFBEC] rounded-[14px] relative">
-              <div class="h-full p-10 bg-white rounded-[14px] shadow absolute top-5 left-0 right-0">
-                <img
-                  class="w-[43.11px] h-[30px]"
-                  src="https://s3-alpha-sig.figma.com/img/f542/ce48/cf243b656c85ab1b46d0d885f9255acf?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pWDfkDYb1QgDKHRGj7PPqfIhBDhmDbhHgnCV~oNElpPkam-DnOAolkqf~JYc~nLMgKJQFHbTES1v4cfO~xBJxJA~ow3uqxkxbdzThLmfL-Tor-2usdVfxrd4hQWFiIJEGcRVqWfdXGfg-frjrN2xrFEpnMz83rdffTR~lMAhHwpLclD3dVPZ72HIVGU07pjYGlp0GISCL9B-NdNctI6mYfTyLGztu-uL~uEu9mHuEAAcb3Bj00fzCjadrBGCfc1y16dmVbVxsUaghyGwUUjW7aYV5j8gZj8TZT6eL8Ojs28IEMAByBVLK5R3M3G5SvAIC3qR-4q4HsRfQc7kmhE7Uw__"
-                  alt="product-image"
-                />
-                <div>
-                  <h1 class="text-slate-900 text-lg font-semibold mt-2">
-                    Ice Mobile 10Gb
-                  </h1>
-                  <h1 class="text-slate-900 text-sm font-normal mt-2">
-                    Up to 100mbit/s
-                  </h1>
-                </div>
-                <div>
-                  <h1 class="text-slate-900 text-2xl font-bold">
-                    299-/ Month
-                  </h1>
-                </div>
-                <div class="flex items-center mt-2">
-                  <div class=" h-14 px-6 py-4 bg-slate-100 rounded-[41px] shadow border border-gray-100 flex justify-center items-center gap-2.5">
-                    <button class="text-slate-900 text-base font-normal">
-                      Add Subscribe
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <PriceCard image={priceOneImg} heading={"Ice Mobile 10Gb"} text={'Up to 100mbit/s'} price={'299'} cardColor={"bg-[#FFFBEC]"}/>
           </div>
           <div class="w-full sm:w-[48%] md:w-[48%] lg:w-[30%] xl:w-[30%] mb-6">
-            <div class="h-[326px] bg-indigo-50 rounded-[14px] relative">
-              <div class="h-full p-10 bg-white rounded-[14px] shadow absolute top-5 left-0 right-0">
-                <img
-                  class="w-[43.11px] h-[30px]"
-                  src="https://s3-alpha-sig.figma.com/img/686b/593f/d91364131e516da9980a2903048f5869?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VkilbrfdqUuHF2N6hQNzaz-JzIkb5zEI7XlVSGl8FfHlPIn-KqfM9sIxg1jDcSIfkOheYnw3cgMfPEyx11uZ~wz4Srx6nt6tbQx0HMnCA9JZkAqtkunNR0eu7hDYdpXWqvb61mMmVCA7NRAXQ9Q6vRYN0BW-kg1K5VUCxfr5LJKNdmpSzuf60Rm2-3HGNZFlz8wsYuH5~K5fCMVD8UFdDvnhv06DnA68qT239i2NIcx2voTT3Dr-qCjBZhTD99V6fC65P10b3w1-pCnMYaVbPhZ~2oqgYASbvptfUi~F9af1ass-6jGTusfaT44OMrIktZs01gcXn2igZhikGDDxEA__"
-                  alt="product-image"
-                />
-                <div>
-                  <h1 class="text-slate-900 text-lg font-semibold mt-2">
-                    Ice Mobile 10Gb
-                  </h1>
-                  <h1 class="text-slate-900 text-sm font-normal mt-2">
-                    Up to 100mbit/s
-                  </h1>
-                </div>
-                <div>
-                  <h1 class="text-slate-900 text-2xl font-bold">
-                    299-/ Month
-                  </h1>
-                </div>
-                <div class="flex justify-center items-center mt-2">
-                  <div class="w-[185px] h-14 px-6 py-4 bg-slate-100 rounded-[41px] shadow border border-gray-100 flex justify-center items-center gap-2.5">
-                    <button class="text-slate-900 text-base font-normal">
-                      Add Subscribe
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <PriceCard image={priceTwoImg} heading={"Telia Mobil 15GB"} text={'Up to 200mbit/s'} price={'350'} cardColor={"bg-[#F9ECFF]"}/>
           </div>
           <div class="w-full sm:w-[48%] md:w-[48%] lg:w-[30%] xl:w-[30%] mb-6">
-            <div class="h-[326px] bg-slate-100 rounded-[14px] relative">
-              <div class="h-full p-10 bg-white rounded-[14px] shadow absolute top-5 left-0 right-0">
-                <img
-                  class="w-[43.11px] h-[30px]"
-                  src="https://s3-alpha-sig.figma.com/img/7d43/1ad6/6f1b056ef1142b12f416a34418532f09?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=quU6om6It0H3Q60f78aUVt9SWzR0I~zgqL8m6rdFvOyf6DKZKxjoFB1VPqcYBwhUUMT~dEbJAeoruAMxjFGfnhkesqpSZThzUxpj1Cg59mwFrN91oeHXrY-CLKnAN7WY9nzXA1wsqnpyv7YuDQ6DBUhoMGyf6tJFj34pdlMD671iXuhLyIpeg9CNUeoHx41eBYrPQzjCjrEH5kktOzuHc4t2K7ixrj8fPbk5enN6VIsUgK5LWuZp8B7kvdT-MNmjIeLlLs7kLD48yCdoK-~lxMxlkbqZ~50HbrU4-0WIUtAQYvy4EiURLQoZjghEW3rcKd3zlLhZ6BXSjubJUxUS5Q__"
-                  alt="product-image"
-                />
-                <div>
-                  <h1 class="text-slate-900 text-lg font-semibold mt-2">
-                    Ice Mobile 10Gb
-                  </h1>
-                  <h1 class="text-slate-900 text-sm font-normal mt-2">
-                    Up to 100mbit/s
-                  </h1>
-                </div>
-                <div>
-                  <h1 class="text-slate-900 text-2xl font-bold">
-                    299-/ Month
-                  </h1>
-                </div>
-                <div class="flex justify-center items-center mt-2">
-                  <div class="w-[185px] h-14 px-6 py-4 bg-slate-100 rounded-[41px] shadow border border-gray-100 flex justify-center items-center gap-2.5">
-                    <button class="text-slate-900 text-base font-normal">
-                      Add Subscribe
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <PriceCard image={priceThreeImg} heading={"Ice Mobile 10Gb"} text={'Up to 400mbit/s'} price={'400'} cardColor={"bg-[ECEEFF]"}/>
           </div>
         </div>
 
@@ -137,3 +57,37 @@ const Pricing = () => {
 };
 
 export default Pricing;
+
+
+const PriceCard = ({image,heading="",text="",price="200",cardColor="bg-[#FFFBEC]"}) => {
+  return (
+    <div class={`h-[326px] ${cardColor} rounded-[14px] relative`}>
+      <div class="h-full p-10 bg-white rounded-[14px] shadow absolute top-5 left-5 right-0">
+
+
+        <Image src={image} alt="pricing"
+          width={'auto'}
+          height={'auto'}
+        />
+        <div className="flex flex-col gap-1  mt-4">
+          <h1 class="text-slate-900 text-lg font-bold">
+            {heading}
+          </h1>
+          <div class="text-[#171435] text-sm font-semibold">
+            {text}
+          </div>
+        </div>
+        <div>
+          <div class="text-slate-900 text-2xl font-bold mt-4">
+            ₹ {price} {" "} <span className="text-lg font-normal">/ month</span>
+          </div>
+        </div>
+        <div class="flex items-center mt-10">
+          <button class="text-slate-900 text-base font-semibold  h-14 px-6 py-2 bg-slate-100 rounded-[41px] shadow border border-gray-100 flex justify-center items-center gap-2.5">
+            Add Subscription
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
