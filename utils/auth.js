210ba1cc8cb3f596cf80b0b0c2239a4223439bfe
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-03-05 16:38:40 +0530

    add: axios intance and set token

--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,6 @@
 export const setToken=(token)=>{
+    
+    // localStorage.setItem("token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZTZmNjkxNmNmZjI3ZjEzZjE4M2EyNyIsImlhdCI6MTcwOTYzNjIxMCwiZXhwIjoxNzA5NzIyNjEwfQ.uma8xD4ZdUYObPYni7Oe1R9Lp13tc-YFDi1nu1eSOwE")
     localStorage.setItem("token",token)
 }
 
