30332f8b6f617c940b9ce7a1b53340e1838dd585
suraj <surajgautam56878@gmail.com>
2024-03-11 11:59:41 +0530

    signup validation almost done

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -8,31 +8,35 @@ import { signup } from "../../../api/userApi";
 import { useState } from "react";
 import { useDispatch } from 'react-redux';
 import { createAccount } from "../../../redux/reducers/userSlice";
+import * as Yup from 'yup';
 
 const Signup = () => {
     const router = useRouter();
     const dispatch = useDispatch()
 
     const [formValues, setFormValues] = useState({ fName: '', lName: '', email: '', password: '', confirmPassword: '', isAdmin: "" });
+    const [errors, setErrors] = useState({});
 
-    const handleChecked = (e) =>{
-        setFormValues({...formValues, isAdmin: e.target.checked})
+    const validationSchema = Yup.object({
+        fName: Yup.string().required("First Name is required"),
+        lName: Yup.string().required("Last Name is required"),
+        email: Yup.string().required("Email is required").email("Invalid Email"),
+        password: Yup.string().required("Password is required").min(8, "Password must be at least 8 characters"),
+        confirmPassword: Yup.string().required("Confirm Password is required").oneOf([Yup.ref('password'), null], 'Passwords must match'),
+    })
+
+    const handleChecked = (e) => {
+        setFormValues({ ...formValues, isAdmin: e.target.checked })
     }
 
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        const newObject = { ...formValues, [name]: value };
-        setFormValues({ ...newObject });
+        setFormValues({ ...formValues, [name]: value });
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
-        if (!(formValues.fName && formValues.lName && formValues.email && formValues.password && formValues.confirmPassword)) {
-            alert("Please fill all the fields")
-            return
-        }
 
         const apiData = {
             "email": formValues.email,
@@ -42,16 +46,26 @@ const Signup = () => {
             "lastName": formValues.lName,
             "isAdmin": formValues.isAdmin
         }
-
+// Test@1234
         try {
+
+            await validationSchema.validate(formValues, { abortEarly: false });
+            console.log("Form Submitted", formValues);
             const response = await signup(apiData)
 
             if (response.success) {
                 dispatch(createAccount(apiData))
                 router.push("/otp")
+                setErrors({})
             }
         } catch (error) {
-            console.log(error)
+            const newErrors = {};
+
+            error.inner.forEach((err) => {
+                newErrors[err.path] = err.message;
+            });
+
+            setErrors(newErrors);
         }
     }
 
@@ -83,7 +97,7 @@ const Signup = () => {
                 </div>
 
                 <div className=" w-full  flex-[1.3] flex flex-col items-center justify-center ">
-                    <form onSubmit={handleSubmit}>
+                    <form className="form" onSubmit={handleSubmit}>
                         <div className="flex flex-col justify-center gap-8 p-16 w-full h-full ">
                             <div className="text-3xl pb-4">
                                 Create your account
@@ -97,6 +111,7 @@ const Signup = () => {
                                         value={formValues.fName}
                                         onChange={handleChange}
                                     />
+                                    {errors.fName && <div className="error text-xs pl-1 pt-1 text-red-600 font-semibold">{errors.fName}</div>}
                                 </div>
 
                                 <div>
@@ -106,6 +121,7 @@ const Signup = () => {
                                         value={formValues.lName}
                                         onChange={handleChange}
                                     />
+                                    {errors.lName && <div className="error text-xs pl-1 pt-1 text-red-600 font-semibold">{errors.lName}</div>}
                                 </div>
                             </div>
 
@@ -117,6 +133,7 @@ const Signup = () => {
                                     value={formValues.email}
                                     onChange={handleChange}
                                 />
+                                {errors.email && <div className="error text-xs pl-1 pt-1 text-red-600 font-semibold">{errors.email}</div>}
                             </div>
 
                             <div>
@@ -126,6 +143,7 @@ const Signup = () => {
                                     value={formValues.password}
                                     onChange={handleChange}
                                 />
+                                {errors.password && <div className="error text-xs pl-1 pt-1 text-red-600 font-semibold">{errors.password}</div>}
                             </div>
 
                             <div>
@@ -135,13 +153,12 @@ const Signup = () => {
                                     value={formValues.confirmPassword}
                                     onChange={handleChange}
                                 />
-
+                                {errors.confirmPassword && <div className="error text-xs pl-1 pt-1 text-red-600 font-semibold">{errors.confirmPassword}</div>}
                             </div>
 
                             <div className="flex items-center justify-start gap-2">
-                                <input type="checkbox" className="size-6" checked={formValues.isAdmin} name="isAdmin"  onChange={handleChecked} />
+                                <input type="checkbox" className="size-6" checked={formValues.isAdmin} name="isAdmin" onChange={handleChecked} />
                                 <Label className=" font-semibold" >Join as property owner </Label>
-
                             </div>
                             <Button className="w-[450px] h-[60px] rounded-xl  bg-neutral-700 hover:bg-neutral-900" onClick={handleSubmit}>Sign up</Button>
                         </div>
