3b993e935c0576917795df1dc5d69116e71b2837
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-03-12 15:25:54 +0530

    add: design changed

--- api/hotelApi.js
+++ api/hotelApi.js
@@ -1,25 +1,20 @@
 import axios from "./axios";
 
-const headerData =
-{
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}
-
 export const createHotelApi = async (data) => {
-    const response = await axios.post(`/createHotel`, data, headerData);
-    if (!response.statusText === "OK") {
+    const response = await axios.post(`/createHotel`, data);
+    if (!response?.statusText === "OK") {
         throw new Error("Something is wrong.");
     }
-    return response.data
+    return response?.data
 }
 
 export const getAdminHotelsApi = async () => {
-    const response = await axios.get(`/getMyHotels`, headerData);
-    if (!response.statusText === "OK") {
+    const response = await axios.get(`/getMyHotels`);
+    console.log(response)
+    if (!response?.statusText === "OK") {
         throw new Error("Something is wrong.");
     }
-    return response.data
+    return response?.data
+
 }
 
