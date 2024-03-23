78789381906a5ccb286bc47a74167109795240fa
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-03-23 18:05:56 +0530

    fix: rebase 1

--- components/Landing/Header.jsx
+++ components/Landing/Header.jsx
@@ -7,8 +7,13 @@ import { useSelector } from "react-redux";
 
 const Header = () => {
     const router = useRouter();
+<<<<<<< HEAD
     const isAdmin = useSelector((state) => state?.user?.isAdmin)
     console.log(isAdmin, "admin");
+=======
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZWQ2MzJlZjgzOTYyMGU0MTQ1ZjM0MiIsImlhdCI6MTcxMDY3MDE1MCwiZXhwIjoxNzEwNzU2NTUwfQ.JokOZ_MZiz9GqYC3GhQocM7-736PgrgPBwBxY8YDN0w"
+
+>>>>>>> 02778c1 (room: room info page and and room listin page)
     return (
 
         <div className="bg-[#265fe5]">
