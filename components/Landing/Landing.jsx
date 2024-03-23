973b7cbd53c6348db81dcc71855cf84ad5abd04c
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-03-23 17:49:44 +0530

    Update Landing.jsx

--- components/Landing/Landing.jsx
+++ components/Landing/Landing.jsx
@@ -1,31 +1,23 @@
+"use client"
 import React from 'react'
 import Footer from '../Footer/Footer'
 import Offer from './Offer'
 import FourBoxes from './FourBoxes'
-import Main from './Main'
 import Pricing from './Pricing'
 import Content from './Content'
 import Cards from './Cards'
 import Growth from './Growth'
-import RoomInfo from './RoomInfo'
 import Header from './Header'
-import RoomListing from './RoomListing'
-
-
-
 
 const Landing = () => {
     return (
         <>
-           <RoomInfo/>
-           <RoomListing/>
-            <Header/>
-            <Main />
+            <Header />
             <Offer />
             <Pricing />
             <Content />
-            <Growth/>
-            <Cards/>
+            <Growth />
+            <Cards />
             <FourBoxes />
             <Footer />
         </>
