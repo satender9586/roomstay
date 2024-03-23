4c9f90fae3093268c34c14da2d2a61397dcbfea2
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-03-23 18:07:12 +0530

    fix: rebase 3

--- components/Landing/Header.jsx
+++ components/Landing/Header.jsx
@@ -4,16 +4,12 @@ import Image from "next/image"
 import roomstayLogo from "../../assests/official/roomstay.png"
 import { useRouter } from "next/navigation";
 import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 const Header = () => {
     const router = useRouter();
-<<<<<<< HEAD
     const isAdmin = useSelector((state) => state?.user?.isAdmin)
     console.log(isAdmin, "admin");
-=======
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZWQ2MzJlZjgzOTYyMGU0MTQ1ZjM0MiIsImlhdCI6MTcxMDY3MDE1MCwiZXhwIjoxNzEwNzU2NTUwfQ.JokOZ_MZiz9GqYC3GhQocM7-736PgrgPBwBxY8YDN0w"
-
->>>>>>> 02778c1 (room: room info page and and room listin page)
     return (
 
         <div className="bg-[#265fe5]">
@@ -23,6 +19,17 @@ const Header = () => {
                     <span className="text-xl cursor-pointer">RoomStay</span>
                 </div>
                 <div className=" sm:flex space-x-4">
+
+                    {
+                        isAdmin ? (
+                            <Button className="rounded-full" onClick={() => router.push('/dashboard')}>Dashboard</Button>
+                        ) : (
+                            <>
+                                <Button variant="secondary" onClick={() => router.push('/login')}>Login</Button>
+                                <Button onClick={() => router.push('/signup')}>Signup</Button>
+                            </>
+                        )
+                    }
 
                     {
                         isAdmin ? (
