a6f6d01349f8300f03da5685ce04da5f8320cf70
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-04-01 14:00:41 +0530

    add: toast and rooms page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react'
-import Footer from '../Footer/Footer'
+import Footer from '@/components/Footer/Footer'
 import Offer from './Offer'
 import FourBoxes from './FourBoxes'
 import Pricing from './Pricing'
