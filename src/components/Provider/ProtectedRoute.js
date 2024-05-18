e792d69a1ac8bb54d0b10fa790b693015a6551ee
suraj <surajgautam56878@gmail.com>
2024-05-18 13:26:47 +0530

    add:order route in protected

--- src/components/Provider/ProtectedRoute.js
+++ src/components/Provider/ProtectedRoute.js
@@ -1,42 +1,44 @@
-"use client";
-import { useEffect, useState } from "react";
-import { tokenVerification } from "../../../api/userApi";
-import { getUserToken } from "../../../utils/cookies";
-import { useRouter } from "next/navigation";
-import { giveUserSliceObj } from "../../../utils/sliceMethod";
-import { useDispatch } from "react-redux";
-import { setUserSlice } from "../../../redux/reducers/userSlice";
+"use client"
+import { useEffect, useState } from "react"
+import { tokenVerification } from "../../../api/userApi"
+import { getUserToken } from "../../../utils/cookies"
+import { useRouter } from "next/navigation"
+import { giveUserSliceObj } from "../../../utils/sliceMethod"
+import { useDispatch } from "react-redux"
+import { setUserSlice } from "../../../redux/reducers/userSlice"
 
 export const ADMINROUTEOBJ = Object.freeze({
   DASHBOARD: "/dashboard",
   BILL: "/bill",
-});
+  ROOMS: "/rooms",
+})
 
 export const USERROUTEOBJ = Object.freeze({
   LANDING: "/",
-});
+  ROOMS: "/rooms",
+})
 
 const UNPROTECTEDROUTEOBJ = Object.freeze({
   LANDING: "/",
   LOGIN: "/login",
   SIGN_UP: "/signup",
   FORGET: "/forget",
-  FORGET: "/order",
-});
+  ROOMS: "/rooms",
+})
 
 //check if you are on the client (browser) or server
-const isBrowser = () => typeof window !== "undefined";
+const isBrowser = () => typeof window !== "undefined"
 
 const ProtectedRoute = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
-  const dispatch = useDispatch();
-  const token = getUserToken();
+  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+  const dispatch = useDispatch()
+  const token = getUserToken()
 
   const fetchUser = async () => {
     if (token) {
       try {
-        const response = await tokenVerification();
+        const response = await tokenVerification()
 
         // If response is success then check if user is admin or user.
         // If user is admin then find in admin route check function.
@@ -47,58 +49,58 @@ const ProtectedRoute = ({ children }) => {
           // Set user details in redux ( User Slice )
 
           if (response?.user) {
-            const userObj = giveUserSliceObj(response?.user);
-            dispatch(setUserSlice(userObj));
+            const userObj = giveUserSliceObj(response?.user)
+            dispatch(setUserSlice(userObj))
           }
 
           if (response?.user?.isAdmin) {
-            adminProtectedRoutesFun();
+            adminProtectedRoutesFun()
           } else {
-            userProtectedRoutesFun();
+            userProtectedRoutesFun()
           }
         } else {
-          unprotecedRoutesFun();
+          unprotecedRoutesFun()
         }
       } catch (error) {
-        console.log(error, "error");
+        console.log(error, "error")
         // check in unprotected routes
-        unprotecedRoutesFun();
+        unprotecedRoutesFun()
       }
 
-      setIsLoading(false);
+      setIsLoading(false)
     } else {
-      unprotecedRoutesFun();
-      setIsLoading(false);
+      unprotecedRoutesFun()
+      setIsLoading(false)
     }
-  };
+  }
 
   const adminProtectedRoutesFun = () => {
     // Admin logged In
-    let myRoutes = [ADMINROUTEOBJ.DASHBOARD, ADMINROUTEOBJ.BILL];
+    let myRoutes = [ADMINROUTEOBJ.DASHBOARD, ADMINROUTEOBJ.BILL]
 
-    let currentPath = location.pathname;
-    let pathNotFound = myRoutes.indexOf(currentPath) === -1;
+    let currentPath = location.pathname
+    let pathNotFound = myRoutes.indexOf(currentPath) === -1
 
     //If path not found then redirect admin to dashboard
     if (isBrowser() && pathNotFound) {
-      router.replace(ADMINROUTEOBJ.DASHBOARD);
+      router.replace(ADMINROUTEOBJ.DASHBOARD)
     }
-  };
+  }
 
   const userProtectedRoutesFun = () => {
     // User logged In
-    let myRoutes = [USERROUTEOBJ.LANDING];
+    let myRoutes = [USERROUTEOBJ.LANDING]
 
-    let currentPath = location.pathname;
+    let currentPath = location.pathname
 
-    let pathNotFound = myRoutes.indexOf(currentPath) === -1;
+    let pathNotFound = myRoutes.indexOf(currentPath) === -1
 
-    console.log(currentPath, pathNotFound, myRoutes, "currentPath");
+    console.log(currentPath, pathNotFound, myRoutes, "currentPath")
     // When path not found in my routes make it redirect to index page
     if (isBrowser() && pathNotFound) {
-      router.replace(UNPROTECTEDROUTEOBJ.LANDING);
+      router.replace(UNPROTECTEDROUTEOBJ.LANDING)
     }
-  };
+  }
 
   const unprotecedRoutesFun = () => {
     // User not logged In
@@ -107,28 +109,29 @@ const ProtectedRoute = ({ children }) => {
       UNPROTECTEDROUTEOBJ.LOGIN,
       UNPROTECTEDROUTEOBJ.SIGN_UP,
       UNPROTECTEDROUTEOBJ.FORGET,
-    ];
+      UNPROTECTEDROUTEOBJ.ROOMS,
+    ]
 
-    let currentPath = location.pathname;
+    let currentPath = location.pathname
 
-    let pathNotFound = myRoutes.indexOf(currentPath) === -1;
+    let pathNotFound = myRoutes.indexOf(currentPath) === -1
 
     // When path not found in my routes make it redirect to index page
     if (isBrowser() && pathNotFound) {
-      router.replace(UNPROTECTEDROUTEOBJ.LANDING);
+      router.replace(UNPROTECTEDROUTEOBJ.LANDING)
     }
-  };
+  }
 
   useEffect(() => {
     // Fetch user details when render
-    fetchUser();
-  }, []);
+    fetchUser()
+  }, [])
 
   if (isLoading) {
-    return null; // Ignore this loading or you can implement loading functionality
+    return null // Ignore this loading or you can implement loading functionality
   } else {
-    return children;
+    return children
   }
-};
+}
 
-export default ProtectedRoute;
+export default ProtectedRoute
